bc423da08ab6e95fc6b6aefc7e26bfae56674613
Christoffer Nielsen <christoffer.nielsen@se.experis.com>
2021-08-31 13:39:37 +0200

    build(project-model): Created spring with hibernate project and adding project-model

---
+++ legalt-backend/src/main/java/com/example/legaltbackend/LegaltBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.legaltbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegaltBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegaltBackendApplication.class, args);
+    }
+
+}
---
+++ legalt-backend/src/main/java/models/Project.java
@@ -0,0 +1,51 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    private byte[] image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
