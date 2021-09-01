b5c2f8b163696702ce475142340705ae17b51d7a
Christoffer Nielsen <christoffer.nielsen@se.experis.com>
2021-09-01 11:42:02 +0200

    build(postgres): Adding import.sql file for adding data to some database tables

---
+++ legalt-backend/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+--Projects
+INSERT INTO projects (title, description, image) VALUES ('Project Title 1', 'Project Description 1', null);
+INSERT INTO projects (title, description, image) VALUES ('Project Title 2', 'Project Description 2', null);
+INSERT INTO projects (title, description, image) VALUES ('Project Title 3', 'Project Description 3', null);
+INSERT INTO projects (title, description, image) VALUES ('Project Title 4', 'Project Description 4', null);
+INSERT INTO projects (title, description, image) VALUES ('Project Title 5', 'Project Description 5', null);
+--Project_Status
+INSERT INTO project_status (title) VALUES ('Founding');
+INSERT INTO project_status (title) VALUES ('In progress');
+INSERT INTO project_status (title) VALUES ('Stalled');
+INSERT INTO project_status (title) VALUES ('Completed');
+--Roles
+INSERT INTO roles (title) VALUES ('Administrator');
+INSERT INTO roles (title) VALUES ('User');
