62ad70067775baf851849e4d95efa184b85a5e68
wrang308 <markus.wrang_94@hotmail.com>
2021-09-02 09:59:31 +0200

    feat(get projects):now fetching the projects using axios

--- lagalt-react/src/components/views/ProjectComponent.jsx
+++ lagalt-react/src/components/views/ProjectComponent.jsx
@@ -1,20 +1,21 @@
 import { useEffect, useState } from 'react'
 import { Card } from 'react-bootstrap';
 
-const ProjectComponent = ({project}) => {
+const ProjectComponent = ({title, description, projectTags,skills}) => {
     //useState 
-    console.log(project)
+    console.log(title)
+    //console.log(project)
     const user = "user";
     const category = "category";
     const time = "5 h "
-    const skills = ["skill 1", "skill 2", "skill 3", "skill 4", "skill 5",]
+    //const skills = ["skill 1", "skill 2", "skill 3", "skill 4", "skill 5",]
   return (
     <Card className="projectComponent">
         <Card.Body>
             <Card.Text> category: {category} *posted by {user}, {time} ago</Card.Text>
-        <Card.Title>{project}</Card.Title>
+        <Card.Title>{title}</Card.Title>
         <ul className="horizontal-list">
-        {skills.map((item, i) => <li>{item}  </li>)}
+        {skills && skills.map((item, i) => <li>{item}  </li>)}
         </ul>
         </Card.Body>
         <img src="https://source.unsplash.com/1600x900" alt="" />
--- lagalt-react/src/components/views/ProjectView.jsx
+++ lagalt-react/src/components/views/ProjectView.jsx
@@ -3,10 +3,36 @@ import ProjectComponent from './ProjectComponent'
 import ProjectRecomended from './ProjectRecomended'
 import ProjectFilterComponent from './ProjectFilterComponent'
 import './ProjectViewStyle.css'
-
+import axios from 'axios'
 const ProjectView = () => {
+  const [projects, setProjects] = useState(null)
 
-    const projects = ["asd", "qwe", "lol", "kalle", "anka"];
+/*   useEffect(() => {
+    axios.get(`http://localhost:8080/api/v1/projects`)
+  .then(res => {
+      console.log(res)
+    const projects = res.data;
+    this.setProjects(projects);
+
+  });
+  }, []); */
+
+
+  useEffect(() => {
+    axios.get(`http://localhost:8080/api/v1/projects`).then((response) => {
+      console.log(response)
+      setProjects(response.data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  }, []);
+
+  
+
+  //
+
+    //const projects = ["asd", "qwe", "lol", "kalle", "anka"];
 
 
   return (
@@ -16,7 +42,13 @@ const ProjectView = () => {
         <br/>
         <div>Filter projects</div>
         <ProjectFilterComponent/>
-        {projects.map((project, i) => <ProjectComponent project={project} key={i} />)}
+        
+        {projects && projects.map((project, i) => <ProjectComponent
+         title={project.title}
+         description={project.description}
+         projectTags={project.projectTags}
+         skills={project.skills}
+         key={i} />)}
         
     </div>
   )
